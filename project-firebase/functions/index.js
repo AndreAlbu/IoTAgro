2a47b3608ef976b9640f4ef1ac30f96ad66ac311
Pedro Emanuel <pa69731@gmail.com>
2023-04-30 00:35:49 -0300

    feat: Firebase Cloud Notification, notification to alert changes in water pump states

---
+++ project-firebase/functions/index.js
@@ -0,0 +1,53 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const {Expo} = require("expo-server-sdk");
+admin.initializeApp();
+const expo = new Expo();
+
+exports.notifyOnStateChange = functions.database.ref("/estadoBomba")
+    .onUpdate(async (change, context) => {
+      const newValue = change.after.val();
+
+      const payload = {
+        notification: {
+          title: "Estado da bomba alterado",
+          // eslint-disable-next-line max-len
+          body: `A bomba foi ${newValue == 1 ? "ligada" : "Desligada"}, entre já no app e saiba mais`,
+        },
+      };
+
+      const tokensSnapshot = await admin.firestore().collection("tokens").get();
+      const tokens = tokensSnapshot.docs.map((doc) => doc.data().token);
+
+      // Filter out invalid tokens
+      const expoTokens = [];
+      for (const token of tokens) {
+        if (!Expo.isExpoPushToken(token)) {
+          console.error(`Invalid Expo push token ${token}`);
+          continue;
+        }
+        expoTokens.push(token);
+      }
+
+      const messages = expoTokens.map((token) => ({
+        to: token,
+        sound: "default",
+        title: payload.notification.title,
+        body: payload.notification.body,
+      }));
+
+      const chunks = expo.chunkPushNotifications(messages);
+
+      const sendChunks = async () => {
+        for (const chunk of chunks) {
+          try {
+            const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+            console.log(ticketChunk);
+          } catch (error) {
+            console.error(`Error sending notification: ${error}`);
+          }
+        }
+      };
+      await sendChunks();
+      return null;
+    });
